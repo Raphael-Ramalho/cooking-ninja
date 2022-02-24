0247e9b3a68f8407690e22b824ceb030d1c3763c
Raphael Ramalho <rsramalhovf@gmail.com>
2022-02-24 11:59:07 -0300

    update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # cooking-ninja
  firebase aplication
+
+
+# How to start
+
+npm install
+npx json-server --watch ./data/db.json
+npm run start
--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+
+
+//components
+import {Home} from './pages/home'
+import {Create} from './pages/create'
+import {Search} from './pages/search'
+import {Recipe} from './pages/recipe'
+
+//styles
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="/create">
+            <Create/>
+          </Route>
+          <Route path="/search">
+            <Search/>
+          </Route>
+          <Route path="/recipes/:id">
+            <Recipe/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
